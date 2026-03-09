7bd3898ae74227c69228149541bae20aa8f491dd
dannychemm123 <dannychemm121@gmail.com>
2026-03-10 00:22:20 +0100

    BST

--- binary_trees.py
+++ binary_trees.py
@@ -58,6 +58,43 @@ def iterative_binary_tree(node):
         print(node)
 iterative_binary_tree(A)
 
+print('='*10)
+def search(node,target):
+    if not node:
+        return f'{target} not found'
+    if node.value == target:
+        return f'{target} found'
+    return search(node.left,target) or search(node.right,target)
 
-def BFS(node):
-    
+print(search(A,11))
+# Binary Search Trees (BSTs)
+
+#       5
+#    1    8
+#  -1 3  7 9
+
+A2 = TreeNode(5)
+B2 = TreeNode(1)
+C2 = TreeNode(8)
+D2 = TreeNode(-1)
+E2 = TreeNode(3)
+F2 = TreeNode(7)
+G2 = TreeNode(9)
+
+A2.left, A2.right = B2, C2
+B2.left, B2.right = D2, E2
+C2.left, C2.right = F2, G2
+
+print(A2)
+
+
+def BST(node,target):
+    if not node: 
+        return f'{target} was found'
+    if node.value == target:
+        return f'{target} was found '
+    if target<node.value:
+        return BST(node.left,target)
+    else:
+        BST(node.right,target)
+print(BST(A2,1))
